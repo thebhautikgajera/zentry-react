6748971fb9aa9799c58b0a9c250660ffd86d5e8d
thebhautikgajera <bhautikpatel464@gmail.com>
2024-11-24 14:15:56 +0530

    3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -141,13 +141,13 @@ const Hero = () => {
         </div>
 
         <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
-          G<b>A</b>MING
+          G<b>A</b>MES
         </h1>
 
         <div className="absolute left-0 top-0 z-40 size-full">
           <div className="mt-24 px-5 sm:px-10">
             <h1 className="special-font hero-heading text-blue-100">
-              redefi<b>n</b>e
+              Z<b>e</b>ntry
             </h1>
 
             <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
@@ -165,7 +165,7 @@ const Hero = () => {
       </div>
 
       <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
-        G<b>A</b>MING
+        G<b>A</b>MES
       </h1>
     </div>
   );
